d3d16fcd3e3c63114e3b5cc7eb344b798253d85a
Brooklyn Huang <brooklyn.huang@sap.com>
2014-09-03 16:58:46 +0800

    Make the scaffold.
    1.Create phonegap project.
    2.Init bower.
    3.Bower install angular.js
    
    Signed-off-by: Brooklyn Huang <brooklyn.huang@sap.com>

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var aApp = angular.module('packageTracking', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ajoslin.mobile-navigate', 'ngMobile'])
+	.config(function($compileProvider) {
+		$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+	})
+	.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.when('/', {
+				templateUrl: 'partials/homeView.html',
+				controller: 'HomeCtrl'
+			});
+			$routeProvider.when('/view1', {
+				templateUrl: 'partials/notificationView.html'
+			});
+			$routeProvider.when('/view2', {
+				templateUrl: 'partials/geolocationView.html'
+			});
+			$routeProvider.when('/view3', {
+				templateUrl: 'partials/accelerometerView.html'
+			});
+			$routeProvider.when('/view4', {
+				templateUrl: 'partials/deviceInfoView.html'
+			});
+			$routeProvider.when('/view5', {
+				templateUrl: 'partials/cameraView.html'
+			});
+			$routeProvider.when('/view6', {
+				templateUrl: 'partials/contactsView.html'
+			});
+			$routeProvider.when('/view7', {
+				templateUrl: 'partials/compassView.html'
+			});
+			$routeProvider.when('/view8', {
+				templateUrl: 'partials/hackerNewsView.html'
+			});
+			$routeProvider.otherwise({
+				redirectTo: '/'
+			});
+		}
+	]);
