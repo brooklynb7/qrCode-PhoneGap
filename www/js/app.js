f9bb997933ea821413839f05ecdcfd115979efbe
Brooklyn Huang <brooklyn.huang@sap.com>
2014-09-05 17:42:10 +0800

    Add the ngTouch module
    Signed-off-by: Brooklyn Huang <brooklyn.huang@sap.com>

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var aApp = angular.module('packageTracking', ['ngRoute', 'packageTrackingControllers']);
+var aApp = angular.module('packageTracking', ['ngRoute','ngTouch', 'packageTrackingControllers']);
 
 aApp.config(['$routeProvider',
 	function($routeProvider) {
